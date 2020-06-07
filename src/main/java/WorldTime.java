f76a9f76e501b4528494734d6f2815f55f785107
dudo <dlwjdd@naver.com>
2020-06-07 14:01:36 +0900

    WorldTime 1차수정

--- src/main/java/WorldTime.java
+++ src/main/java/WorldTime.java
@@ -1,42 +1,39 @@
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.*;
 
-/**
- * 
- */
 public class WorldTime extends Mode {
 
-    /**
-     * Default constructor
-     */
+    private String[] city = {"NYC", "SYD", "LAX", "LON"};
+    private int[] timeDifference = {14, 1, 16, 8};
+    private int index = -1;
+
     public WorldTime() {
     }
 
-    /**
-     * 
-     */
-    private String city[4];
-
-    /**
-     * 
-     */
-    private String timeDifference[4];
-
-    /**
-     * @param dir 
-     * @return
-     */
-    public LocalDateTime getWorldTime(int dir) {
+    // LocalDateTime으로 현재 시간을 받고, city로 현재 도시를 받습니다. -> 수정했어
+    public LocalDateTime getWorldTime(LocalDateTime dir, String city) {
         // TODO implement here
-        return null;
+        switch(city){
+            case "NYC":
+                this.index = 0;
+            case "SYD":
+                this.index = 1;
+            case "LAX":
+                this.index = 2;
+            case "LON":
+                this.index = 3;
+            default:
+                System.out.println("Wrong City name");
+        }
+        return dir.plusHours(timeDifference[index]);
     }
 
-    /**
-     * @return
-     */
-    public Set<String> getCity() {
+    public String getCity() {
         // TODO implement here
-        return null;
+        return city[this.index];
     }
 
 }
