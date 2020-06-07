e444873ec11ac7aad73a8688205d05a525cb4e77
dudo <dlwjdd@naver.com>
2020-06-07 15:54:59 +0900

    WorldTime 1차 수정

--- src/main/java/WorldTime.java
+++ src/main/java/WorldTime.java
@@ -14,19 +14,23 @@ public class WorldTime extends Mode {
     }
 
     // LocalDateTime으로 현재 시간을 받고, city로 현재 도시를 받습니다. -> 수정했어
-    public LocalDateTime getWorldTime(LocalDateTime dir, String city) {
+    public LocalDateTime getWorldTime(LocalDateTime dir, String upOrDown) {
         // TODO implement here
-        switch(city){
-            case "NYC":
-                this.index = 0;
-            case "SYD":
-                this.index = 1;
-            case "LAX":
-                this.index = 2;
-            case "LON":
-                this.index = 3;
-            default:
-                System.out.println("Wrong City name");
+        switch(upOrDown){
+            case "UP":
+                if(this.index < 3){
+                    this.index++;
+                } else{
+                    this.index = 0;
+                }
+                break;
+            case "DOWN":
+                if(this.index > 0){
+                    this.index--;
+                } else{
+                    this.index = 3;
+                }
+                break;
         }
         return dir.plusHours(timeDifference[index]);
     }
