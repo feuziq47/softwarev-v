9153c97432476815dfe7598cf4b1f9ac9bb1f4fd
kdje0506 <48521269+kdje0506@users.noreply.github.com>
2020-06-08 20:11:54 +0900

    TimeKeeping 주석 추가

--- src/main/java/TEST/TimeKeepingTest.java
+++ src/main/java/TEST/TimeKeepingTest.java
@@ -11,13 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TimeKeepingTest {//
     /**
-     * startStopwatch을 했을때, 시간이 startTime이 증가 되었는지 확인
-     * 콜백 함수로 인하여, System 단에서 test해야 한다.
-     * 이하 동문
-     *
-     * Stopwatch가 0초부터 시작되는지 Test한다.
-     * Stopwatch의 시간이 시간의 흐름에 따라 제대로 증가하는 지 Test한다.
-     *
+     *기존의 시간에서 설정한 시간으로 현재 시간이 제대로 변경되는지 Test한다.
      */
     @Test
     public void setCurrentTime(){
