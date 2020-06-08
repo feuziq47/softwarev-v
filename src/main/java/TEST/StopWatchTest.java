51eadde1db4bac823e1a633f90ac4dc6bfc5c482
kdje0506 <48521269+kdje0506@users.noreply.github.com>
2020-06-08 20:10:19 +0900

    test 스탑워치, 타임 추가

--- src/main/java/TEST/StopWatchTest.java
+++ src/main/java/TEST/StopWatchTest.java
@@ -1,5 +1,6 @@
 package TEST;
 
+import RDM.RDMSystem;
 import RDM.StopWatch;
 import RDM.Timer;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,7 @@ public class StopWatchTest {//
      */
     @Test
     public void startStopwatch() throws InterruptedException {
+        RDMSystem rdms = new RDMSystem();
         StopWatch sw = new StopWatch();
         sw.startStopwatch();
         TimeUnit.SECONDS.sleep(3);
@@ -52,6 +54,19 @@ public class StopWatchTest {//
      */
     @Test
     public void clearStopwatch(){
+
+    }
+
+    /**
+     * startStopwatch을 했을때, 시간이 startTime이 증가 되었는지 확인
+     * 콜백 함수로 인하여, System 단에서 test해야 한다.
+     * 이하 동문
+     *
+     * Stopwatch에서 Lap Time에 해당하는 신호가 주어졌을때
+     * Stopwatch에 표시된 현재 시간이 기록되는지 test한다.
+     */
+    @Test
+    public void recordLapTime(){
 
     }
 
--- src/main/java/TEST/TimeKeepingTest.java
+++ src/main/java/TEST/TimeKeepingTest.java
@@ -1,4 +1,27 @@
 package TEST;
 
+import RDM.RDMSystem;
+import RDM.StopWatch;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TimeKeepingTest {//
+    /**
+     * startStopwatch을 했을때, 시간이 startTime이 증가 되었는지 확인
+     * 콜백 함수로 인하여, System 단에서 test해야 한다.
+     * 이하 동문
+     *
+     * Stopwatch가 0초부터 시작되는지 Test한다.
+     * Stopwatch의 시간이 시간의 흐름에 따라 제대로 증가하는 지 Test한다.
+     *
+     */
+    @Test
+    public void setCurrentTime(){
+
+
+    }
 }
